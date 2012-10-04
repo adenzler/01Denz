5039d8eddf7c8397a2177694b0ca7033b6f89f6d
Hubert Bonnisseur de la Batte <arnodenzler@hotmail.com>
2012-10-04 12:55:05 +0200

    jquery

---
+++ monsite/Public/Scripts/app.js
@@ -0,0 +1,9 @@
+function check_name(name){
+	if( name == "gabriel"){
+		console.log("Win !");
+	}else if( name == "Clement"){
+		console.log("We find Clement !");
+	}else{
+		console.log("Truth !"+ name + " is different from gabriel and Clement !");
+	}
+}
